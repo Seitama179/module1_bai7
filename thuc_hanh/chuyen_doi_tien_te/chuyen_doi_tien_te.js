4e87d38385e77d81073e630d16d180eec334892e
Seitama179 <ngochd997@gmail.com>
2024-04-09 15:01:03 +0700

    add file currency converter

---
+++ thuc_hanh/chuyen_doi_tien_te/chuyen_doi_tien_te.js
@@ -0,0 +1,12 @@
+function convert() {
+    let amount = +document.getElementById("amount").value;
+    let from = +document.getElementById("from").value;
+    let to = +document.getElementById("to").value;
+    // tinh toan
+//     ket qua = amount* ty gia
+//     ty gia: from/to
+    let rate = from/to;
+    let result = amount*rate;
+//     hien thi ket qua
+    document.getElementById("result").innerText = result;
+}
